1653978c50456b50e6538ef95f02a2ebd4e94512
isadov <lovelas123123@gmail.com>
2015-06-02 12:31:53 +0300

    update

--- src/day11/PrintStreamData.java
+++ src/day11/PrintStreamData.java
@@ -9,7 +9,7 @@ public class PrintStreamData {
 
         String filename = "D:\\JAVA_CLASS\\test.txt";
 
-        try(FileInputStream inputStream = new FileInputStream(filename)) {
+        try (FileInputStream inputStream = new FileInputStream(filename)) {
             printStreamFromFile(inputStream);
         } catch (IOException e) {
             e.printStackTrace();
@@ -21,8 +21,8 @@ public class PrintStreamData {
         StringBuilder builder = new StringBuilder();
         int i;
 
-        try{
-            while((i = fileInputStream.read()) != -1) {
+        try {
+            while ((i = fileInputStream.read()) != -1) {
                 builder.append((char) i);
             }
         } catch (IOException e) {
