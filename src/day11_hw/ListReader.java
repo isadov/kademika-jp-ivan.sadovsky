207cbda8bae29e53b0ee44d3ad601ca3eb207d1f
isadov <lovelas123123@gmail.com>
2015-07-08 12:52:06 +0300

    update

--- src/day11_hw/ListReader.java
+++
@@ -1,9 +0,0 @@
-package day11_hw;
-
-import java.io.IOException;
-import java.util.ArrayList;
-
-public interface ListReader {
-
-    public ArrayList <Profile> read(String fileName) throws IOException;
-}
--- src/day11_hw/ListWriter.java
+++
@@ -1,8 +0,0 @@
-package day11_hw;
-
-import java.util.ArrayList;
-
-public interface ListWriter {
-
-    public void write(String fileName, ArrayList<Profile> profiles);
-}
