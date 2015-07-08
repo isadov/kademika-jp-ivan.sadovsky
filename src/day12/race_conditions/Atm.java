e0f74018359a2087a3a3ed751df7e2ed454cd850
isadov <lovelas123123@gmail.com>
2015-07-08 18:14:33 +0300

    update

--- src/day12/race_conditions/Atm.java
+++
@@ -1,8 +0,0 @@
-package day12.race_conditions;
-
-public interface Atm {
-
-    public void checkBalance(long accountid);
-
-    public void withdrawMoney(long acoountId, int amount);
-}
--- src/day12/theory/MyRunnable.java
+++
@@ -1,9 +0,0 @@
-package day12.theory;
-
-public class MyRunnable implements Runnable {
-    @Override
-    public void run() {
-        System.out.println("Running " + Thread.currentThread().getName() + " " + this.getClass().getSimpleName());
-
-    }
-}
